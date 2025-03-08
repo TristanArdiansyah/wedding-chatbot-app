2534db98348e99c2ef7e869d1dac6faba7bb75a2
Tristan Ardiansyah <nandatristan@gmail.com>
2025-03-08 09:33:05 +0700

    fix bubble chat stream issue

--- app/components/ChatContainer.tsx
+++ app/components/ChatContainer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react';
 import { ChatMessage } from '../page';
+import ChatBubble from './ChatBubble';
 
 interface ChatContainerProps {
   chatHistory: ChatMessage[];
@@ -8,21 +9,25 @@ interface ChatContainerProps {
 
 export default function ChatContainer({ chatHistory }: ChatContainerProps) {
   return (
-    <div className="flex flex-col gap-3">
-      {chatHistory.map((chat, index) => (
-        <div key={index} className="flex flex-col gap-1">
-          {chat.query.trim() !== "" && (
-            <div className="self-end max-w-[80%] bg-[#DBB479] text-black rounded-tl-2xl rounded-br-2xl px-4 py-2 animate-fadeIn break-words">
-              {chat.query}
-            </div>
-          )}
-          {chat.reply.trim() !== "" && (
-            <div className="self-start max-w-[80%] bg-[#335F70] text-white rounded-tr-2xl rounded-bl-2xl px-4 py-2 animate-fadeIn break-words">
-              {chat.reply}
-            </div>
-          )}
-        </div>
-      ))}
+    <div className="flex-grow overflow-y-auto rounded p-4 mb-4">
+
+      <div className="flex flex-col gap-3">
+        {chatHistory.map((chat, index) => (
+          <div key={index} className="flex flex-col gap-2">
+            {chat.query.trim() !== "" && (
+              <div className="self-end max-w-[80%] bg-[#DBB479] text-black rounded-tl-2xl rounded-br-2xl px-4 py-2 animate-fadeIn break-words">
+                {chat.query}
+              </div>
+            )}
+            {chat.reply.trim() !== "" && (
+              <div className="self-start max-w-[80%] bg-[#335F70] text-white rounded-tr-2xl rounded-bl-2xl px-4 py-2 animate-fadeIn break-words">
+                <ChatBubble text={chat.reply} typingSpeed={10} />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
+
   );
 }
